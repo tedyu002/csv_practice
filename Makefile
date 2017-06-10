73d4188a6304164f48a6e5db52cc2e34582908af
Tzu-Te Yu <tedyu002@gmail.com>
2017-06-10 17:58:40 +0800

    Refine test structure.

--- Makefile
+++ Makefile
@@ -2,40 +2,22 @@ LIBS=src/mmap.o src/config.o src/token.o src/type.o src/csv.o src/val.o src/cont
 INCLUDE=-Isrc
 
 CC=gcc
+CVER=-std=c99
 #ASAN=-fsanitize=address -lasan
 #FLTO="-flto"
+DEFS=-D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
 LDFLAGS=${FLTO} ${ASAN}
-CFLAGS=-Wall ${LDFLAGS} -g -std=c99 -Werror -Wpedantic ${INCLUDE} -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
+CFLAGS=-Wall ${LDFLAGS} -g ${CVER} -Werror -Wpedantic ${INCLUDE} ${DEFS}
 
 csv_practice: src/main.o ${LIBS}
 	${CC} ${CFLAGS} src/main.o ${LIBS} -o csv_practice
 
-test: test_config_column_print test_token_string_parse test_csv_row_parse test_csv_csv_parse \
-		test_sort_sort test_cal_cal
+test: src/test.c
+	${CC} ${DEFS} ${CVER} -Werror -Wpedantic  src/test.c -o csv_practice_test
 
-test_config_column_print: test_src/config/column_print.o ${LIBS} test_dir
-	${CC} ${CLFAGS} ${LDFLAGS} test_src/config/column_print.o ${LIBS} -o test/config_column_print
-
-test_token_string_parse: test_src/token/string_parse.o ${LIBS} test_dir
-	${CC} ${CLFAGS} ${LDFLAGS} test_src/token/string_parse.o ${LIBS} -o test/toekn_string_parse
-
-test_csv_row_parse: test_src/csv/row_parse.o ${LIBS} test_dir
-	${CC} ${CFLAGS} ${LDFLAGS} test_src/csv/row_parse.o ${LIBS} -o test/csv_row_parse
-
-test_csv_csv_parse: test_src/csv/csv_parse.o ${LIBS} test_dir
-	${CC} ${CFLAGS} ${LDFLAGS} test_src/csv/csv_parse.o ${LIBS} -o test/csv_csv_parse
-
-test_sort_sort: test_src/sort/sort.o ${LIBS} test_dir
-	${CC} ${CFLAGS} ${LDFLAGS} test_src/sort/sort.o ${LIBS} -o test/sort_sort
-
-test_cal_cal: test_src/cal/cal.o ${LIBS} test_dir
-	${CC} ${CFLAGS} ${LDFLAGS} test_src/cal/cal.o ${LIBS} -o test/cal_cal
-
-test_dir:
-	@mkdir -p test
 
 .PHONY: clean
 clean:
 	@rm -rf src/*.o
-	@rm -rf csv_practice
-	@rm -rf test/
+	@rm -f csv_practice
+	@rm -f csv_practice_test
---
+++ src/test.c
@@ -0,0 +1,182 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 4096
+
+static void traverse(const char *dir_path, int depth);
+static void do_test(DIR *dir, const char *fname, int depth);
+static bool is_same(DIR *dir, const char *f1, const char *f2);
+
+int
+main(int argc, char *argv[])
+{
+	traverse(argv[1], 1);
+
+	return 0;
+}
+
+static void
+traverse(const char *dir_path, int depth)
+{
+	DIR *dir = NULL;
+
+	dir = opendir(dir_path);
+
+	while (true) {
+		struct dirent ent, *tmp_ent = NULL;
+		struct stat st;
+
+		if (readdir_r(dir, &ent, &tmp_ent) > 0) {
+			break;
+		}
+		else if (tmp_ent == NULL) {
+			break;
+		}
+
+		if (ent.d_name[0] == '.') {
+			continue;
+		}
+
+		if (fstatat(dirfd(dir), ent.d_name, &st, 0) == -1) {
+			continue;
+		}
+		if (S_ISDIR(st.st_mode)) {
+			char path[PATH_MAX];
+
+			snprintf(path, sizeof(path), "%s/%s", dir_path, ent.d_name);
+			traverse(path, depth + 1);
+			continue;
+		}
+
+		if (strncmp(ent.d_name, "test_", strlen("test_")) == 0) {
+			const char *p = strstr(ent.d_name, ".conf");
+
+			if (p != NULL && *(p + strlen(".conf")) == '\0') {
+				do_test(dir, ent.d_name, depth);
+			}
+		}
+	}
+
+	closedir(dir);
+}
+
+static void
+do_test(DIR *dir, const char *fname, int depth)
+{
+	pid_t pid = -1;
+
+	pid = fork();
+
+	if (pid > 0) {
+		int status = 0;
+
+		char ans_out[PATH_MAX] = "";
+		char ans_res[PATH_MAX] = "";
+		char ans_err[PATH_MAX] = "";
+
+		wait(&status);
+
+		if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+			printf("%s: Failed\n", fname);
+			return;
+		}
+
+		strcpy(ans_out, fname);
+		strcpy(ans_res, fname);
+		strcpy(ans_err, fname);
+
+		strcat(ans_out, ".out");
+		strcat(ans_res, ".res");
+		strcat(ans_err, ".err");
+
+		if (!is_same(dir, ans_out, "test.out") || !is_same(dir, ans_err, "test.err") || !is_same(dir, ans_res, "test.res")) {
+			printf("%s: Failed\n", fname);
+		}
+		else {
+			printf("%s: Success\n", fname);
+		}
+	}
+	else if (pid == 0) {
+		fchdir(dirfd(dir));
+		char path[PATH_MAX] = "";
+
+		for (int i = 0; i < depth; ++i) {
+			strcat(path, "../");
+		}
+
+		strcat(path, "csv_practice");
+
+		execlp(path, "csv_practice", fname, NULL);
+		exit(1);
+	}
+}
+
+static bool
+is_same(DIR *dir, const char *f1, const char *f2)
+{
+	int fd1 = -1;
+	int fd2 = -1;
+	bool res = false;
+
+	struct stat st1, st2;
+
+	if ((fd1 = openat(dirfd(dir), f1, O_RDONLY)) < 0) {
+		goto end;
+	}
+
+	if ((fd2 = openat(dirfd(dir), f2, O_RDONLY)) < 0) {
+		goto end;
+	}
+
+	if (fstat(fd1, &st1) == -1 || fstat(fd2, &st2) == -1) {
+		goto end;
+	}
+
+	if (st1.st_size == st2.st_size) {
+		char buffer[2][BUFFER_SIZE];
+
+		ssize_t rnd_cnt[2];
+
+		while (true) {
+			rnd_cnt[0] = read(fd1, buffer[0], BUFFER_SIZE);
+			rnd_cnt[1] = read(fd2, buffer[1], BUFFER_SIZE);
+
+			if (rnd_cnt[0] < 0 || rnd_cnt[1] < 0) {
+				break;
+			}
+
+			if (rnd_cnt[0] != rnd_cnt[1]) {
+				break;
+			}
+
+			if (rnd_cnt[0] == 0) {
+				res = true;
+				break;
+			}
+
+			if (memcmp(buffer[0], buffer[1], rnd_cnt[0]) != 0) {
+				break;
+			}
+		}
+	}
+
+
+end:
+	if (fd1 >= 0) {
+		close(fd1);
+	}
+	if (fd2 >= 0) {
+		close(fd2);
+	}
+
+	return res;
+}
+
