4404ee81d576e647a9c61b82578c4acd91ad4ab1
Tzu-Te Yu <tedyu002@gmail.com>
2017-06-11 13:14:24 +0800

    Added 20 column test case and fixed char limits error.

--- src/limits.h
+++ src/limits.h
@@ -3,7 +3,7 @@
 
 #define VARCHAR_MAX_LEN		1024
 #define VARCHAR_MAX_SIZE	(VARCHAR_MAX_LEN + 1)
-#define	CHAR_MAX_LEN		8
+#define CHAR_MAX_LEN			128
 #define CHAR_MAX_SIZE		(CHAR_MAX_LEN + 1)
 #define CSV_HEADER_MAX		20
 
