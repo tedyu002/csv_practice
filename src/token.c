ff4783b849b020173306594e877691cedfec24b8
Tzu-Te Yu <tedyu002@gmail.com>
2017-06-11 23:53:21 +0800

    Handle CRLF case.

--- src/token.c
+++ src/token.c
@@ -58,13 +58,19 @@ token_column_string_get(const char **src, char **string)
 	const char *str_end = NULL;
 	bool quoted = false;
 
+	FILE *target_fp = NULL;
+	size_t target_size = 0;
+	char *target_buf = NULL;
+
+	int save_errno = 0;
+
 	if (src == NULL || string == NULL) {
 		errno = EINVAL;
 		return -1;
 	}
 
 	beg = *src;
-	while (*beg != '\0' && *beg != '\n' && *beg != ',' && *beg == ' ') {
+	while (*beg != '\0' && *beg != '\n' && *beg != ',' && (*beg == ' ' || iscntrl(*beg))) {
 		beg++;
 	}
 
@@ -83,21 +89,20 @@ token_column_string_get(const char **src, char **string)
 		beg++;
 	}
 
+	if ((target_fp = open_memstream(&target_buf, &target_size)) == NULL) {
+		save_errno = ENOMEM;
+		goto end;
+	}
 	if (quoted) {
 		str_end = beg;
-		FILE *target_fp = NULL;
-		size_t target_size = 0;
-		char *target_buf = NULL;
-
-		target_fp = open_memstream(&target_buf, &target_size);
-		if (target_fp == NULL) {
-			return -1;
-		}
 
 		while (*str_end != '\0') {
 			if (*str_end == '"') {
 				if (*(str_end + 1) == '"') {
-					fputc('"', target_fp);
+					if (fputc('"', target_fp) == EOF) {
+						save_errno = ENOMEM;
+						goto end;
+					}
 					str_end += 2;
 				}
 				else {
@@ -105,28 +110,31 @@ token_column_string_get(const char **src, char **string)
 					break;
 				}
 			}
-			else if (*str_end == '\n') {
+			else if (iscntrl(*str_end)) {
 				str_end++;
 			}
 			else {
-				fputc(*str_end, target_fp);
+				if (fputc(*str_end, target_fp) == EOF) {
+					save_errno = ENOMEM;
+					goto end;
+				}
 				str_end++;
 			}
 		}
 
 		fclose(target_fp);
+		target_fp = NULL;
 
 		if (quoted) {
 			*src = str_end;
-			errno = EINVAL;
-			free(target_buf);
-			return -1;
+			save_errno = EINVAL;
+			goto end;
 		}
 
 		*string = target_buf;
 		*src = str_end + 1;
 
-		return 0;
+		target_buf = NULL;
 	}
 	else {
 		char *tmp_str = NULL;
@@ -139,22 +147,23 @@ token_column_string_get(const char **src, char **string)
 			}
 			else if (*col_end == '"') {
 				*src = col_end + 1;
-				errno = EINVAL;
-				return -1;
+				save_errno = EINVAL;
+				goto end;
 			}
 			col_end++;
 		}
 
 		str_end = col_end;
 
-		while (beg < str_end && *(str_end - 1) == ' ') {
+		while (beg < str_end && (*(str_end - 1) == ' ' || iscntrl(*(str_end - 1)))) {
 			str_end--;
 		}
 
 		tmp_str_len = str_end - beg;
 
 		if ((tmp_str = malloc((tmp_str_len + 1) * sizeof(char))) == NULL) {
-			return -1;
+			save_errno = ENOMEM;
+			goto end;
 		}
 
 		strncpy(tmp_str, beg, tmp_str_len);
@@ -162,6 +171,22 @@ token_column_string_get(const char **src, char **string)
 
 		*string = tmp_str;
 		*src = col_end;
+	}
+
+end:
+	if (target_fp != NULL) {
+		fclose(target_fp);
+		target_fp = NULL;
+	}
+
+	if (target_buf != NULL) {
+		free(target_buf);
+		target_buf = NULL;
+	}
+
+	if (save_errno != 0) {
+		errno = save_errno;
+		return -1;
 	}
 
 	return 0;
